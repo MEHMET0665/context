74565d19734564b37f59d3dd13a0db3e6e532cc0
Memet <meavci@gmail.com>
2021-01-23 18:11:49 -0500

    create css file

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react'
 class TodoList extends React.Component{
   render(){
     return(
-      <div className='title-bar'>
+      <div className='todolist'>
       <input type="text"/>
       <ul>
         <li>Go to market</li>
