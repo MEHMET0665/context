e46fc8b72790799bab8cda729c85b19b7ea21a39
Memet <meavci@gmail.com>
2021-01-27 23:13:45 -0500

    complete reducer function

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,39 +1,36 @@
-import React,{useContext,useState} from 'react'
+import React, {useContext, useState} from 'react'
 import {ThemeContext} from '../contexts/ThemeContext';
-import {TodoItemsContext} from '../contexts/TodoItemsContext'
+import {TodoItemsContext} from '../contexts/TodoItemsContext';
 
 function TodoList() {
-  const[title,setTitle]=useState('');
-  const {items,addItem,removeItem}=useContext(TodoItemsContext)
+    const [title, setTitle] = useState('');
+    const {items, dispatch} = useContext(TodoItemsContext);
     const {isLightTheme, lightTheme, darkTheme} = useContext(ThemeContext);
     const theme = isLightTheme ? lightTheme : darkTheme;
 
-    const handleInput=(e)=>{
+    const handleInput = (e) => {
       setTitle(e.target.value)
     }
-    const handleSubmit=(e)=>{
+
+    const handleSubmit = (e) => {
       e.preventDefault();
-      addItem(title)
+      // addItem(title);
+      dispatch({type: 'addItem', title: title})
+      setTitle('')
     }
-    // const handleClick=(e)=>{
-    //   removeItem(e.target.id)
-    // }
+
     return (
       <div className='todolist' style={{backgroundColor: theme.body, color: theme.text}}>
         <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleInput} value={title}/>
+          <input type="text" onChange={handleInput} value={title}/>
         </form>
         <ul>
-          {items.map(item=>{
-            return(<li id={item.id}key={item.id}style={{backgroundColor: theme.nav}} onClick={() => removeItem(item.id)}>{item.title}
-              
-            </li>)
+          {items.map(item => {
+            return <li key={item.id} style={{backgroundColor: theme.nav}} onClick={() => dispatch({type: 'removeItem', id: item.id})}>{item.title}</li>
           })}
-          
         </ul>
       </div>
     )
-  
 }
 
 export default TodoList;
--- src/contexts/TodoItemsContext.js
+++ src/contexts/TodoItemsContext.js
@@ -1,16 +1,19 @@
-import React, {createContext,useState} from'react'
+import React, {createContext,useReducer} from'react'
+import {todoItemReducer} from '../reducers/todoItemReducer'
 export const TodoItemsContext=createContext();
 function TodoItemsContextProvider(props){
-  const [items, setItems]=useState([])
-  const addItem=(title)=>{
-    setItems([...items,{title:title,id:Date.now()}])
-  }
-  const removeItem=(id)=>{
-const newArray=items.filter((item)=>item.id!==id);
-setItems(newArray)
-  }
+
+  const [items, dispatch] = useReducer(todoItemReducer, []);
+//   const [items, setItems]=useState([])
+//   const addItem=(title)=>{
+//     setItems([...items,{title:title,id:Date.now()}])
+//   }
+//   const removeItem=(id)=>{
+// const newArray=items.filter((item)=>item.id!==id);
+// setItems(newArray)
+//   }
   return(
-    <TodoItemsContext.Provider value={{items,addItem:addItem,removeItem:removeItem}}>
+    <TodoItemsContext.Provider value={{items, dispatch/*addItem:addItem,removeItem:removeItem*/}}>
       {props.children}
     </TodoItemsContext.Provider>
   )
