de62695a60c1104c70699c7e288c318a1da21bf9
Memet <meavci@gmail.com>
2021-01-27 22:24:54 -0500

    create addItem function

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,14 +1,25 @@
-import React,{useContext} from 'react'
+import React,{useContext,useState} from 'react'
 import {ThemeContext} from '../contexts/ThemeContext';
 import {TodoItemsContext} from '../contexts/TodoItemsContext'
 
 function TodoList() {
-  const {items}=useContext(TodoItemsContext)
+  const[title,setTitle]=useState('');
+  const {items,addItem}=useContext(TodoItemsContext)
     const {isLightTheme, lightTheme, darkTheme} = useContext(ThemeContext);
     const theme = isLightTheme ? lightTheme : darkTheme;
+
+    const handleInput=(e)=>{
+      setTitle(e.target.value)
+    }
+    const handleSubmit=(e)=>{
+      e.preventDefault();
+      addItem(title)
+    }
     return (
       <div className='todolist' style={{backgroundColor: theme.body, color: theme.text}}>
-        <input type="text"/>
+        <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInput} value={title}/>
+        </form>
         <ul>
           {items.map(item=>{
             return(<li key={item.id}style={{backgroundColor: theme.nav}}>{item.title}
